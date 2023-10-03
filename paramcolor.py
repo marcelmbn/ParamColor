c336c22a72bf868148f707cb5e19d44363bd81f2
Marcel Müller <marcel.mueller@thch.uni-bonn.de>
2023-10-03 16:27:23 +0200

    parse specific element parameter sections
    Signed-off-by: Marcel Müller <marcel.mueller@thch.uni-bonn.de>

--- paramcolor.py
+++ paramcolor.py
@@ -6,6 +6,8 @@ to color-highlight differences between the parameters in the file.
 
 import argparse as ap
 
+# from typing import Dict, List
+
 
 def main():
     """
@@ -19,25 +21,36 @@ def main():
     parser.add_argument(
         "--file", help="The parameter file to be parsed.", type=str, required=True
     )
+    parser.add_argument(
+        "--elements",
+        help="The elements to be compared.",
+        nargs="+",
+        type=int,
+        required=True,
+    )
     args = parser.parse_args()
 
     # Parse the parameter file
-    elements, elements_lineno = parse_line_numbers(args.file)
+    elements, max_floats = parse_line_numbers(args.file)
 
-    for i, element in enumerate(elements):
-        print(element, elements_lineno[i])
+    # for key, value in elements.items():
+    #     print (key, value)
+
+    # Parse the parameters
+    for el in args.elements:
+        parse_parameters(args.file, el, elements, max_floats)
 
 
-def parse_line_numbers(file_path) -> tuple[list, list]:
+def parse_line_numbers(file_path) -> tuple[dict, int]:
     """
     Parses a parameter file and returns a dictionary with the parameters.
     """
     with open(file_path, encoding="UTF-8") as file:
         lines = file.readlines()
 
-    elements = []
-    element_line_numbers = []
-
+    elements = {}
+    max_floats = 0
+    glob_var = True
     k = 0
     for line in lines:
         content = line.strip().split()
@@ -48,6 +61,7 @@ def parse_line_numbers(file_path) -> tuple[list, list]:
             intval = int(content[0])
             if str(intval) == content[0]:
                 isint = True
+                glob_var = False
         except ValueError:
             try:
                 float(content[0])
@@ -56,10 +70,14 @@ def parse_line_numbers(file_path) -> tuple[list, list]:
                 pass
         if isint:
             k += 1
-            elements.append(int(content[0]))
-            element_line_numbers.append(lines.index(line))
+            elements[int(content[0])] = lines.index(line)
         # Else if the line contains a float (and not an integer!), just continue
         elif isfloat:
+            # count the number of floats in the line
+            if not glob_var:
+                nfloats = len(content)
+                if nfloats > max_floats:
+                    max_floats = nfloats
             continue
         # Else if the line is completely empty, it is the end of the file
         elif content[0] == "":
@@ -68,7 +86,30 @@ def parse_line_numbers(file_path) -> tuple[list, list]:
             print(content)
             raise ValueError("Error. The line does not start with an integer or float.")
 
-    return elements, element_line_numbers
+    return elements, max_floats
+
+
+def parse_parameters(file_path, el, elements, max_floats):
+    """
+    Parses a parameter file and returns a dictionary with the parameters.
+    """
+
+    try:
+        lowerbound = elements[el] + 1
+        upperbound = elements[el + 1] - 1
+    except IndexError as exc:
+        raise IndexError("Error. The element is not in the parameter file.") from exc
+
+    with open(file_path, encoding="UTF-8") as file:
+        for i in range(lowerbound):
+            next(file)
+        lines = []
+        for i in range(upperbound + 1 - lowerbound):
+            lines.append(next(file))
+
+    print(lines)
+
+    return None
 
 
 if __name__ == "__main__":
