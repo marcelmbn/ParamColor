0d77c7f311163b6ef2a2e52eb46c105c8e565ce5
Marcel Müller <marcel.mueller@thch.uni-bonn.de>
2023-10-03 14:22:46 +0200

    initial commit
    Signed-off-by: Marcel Müller <marcel.mueller@thch.uni-bonn.de>

---
+++ paramcolor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+def main():
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
+
+
+def function1():
+    print("Hello World!")
